93c16377e279938b80b35f730726767f1a7f8328
dhanweb <851849619@qq.com>
2024-06-21 11:17:15 +0800

    feat: 修改请求响应配置

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,8 +2,11 @@ import axios from 'axios'
 
 // import qs from 'qs'
 import Message from 'vue-m-message'
+import { ElMessage } from 'element-plus'
 import useUserStore from '@/store/modules/user'
+import { Storage } from '@/utils/storage'
 
+Storage.clear()
 const api = axios.create({
   baseURL: (import.meta.env.DEV && import.meta.env.VITE_OPEN_PROXY === 'true') ? '/proxy/' : import.meta.env.VITE_APP_API_BASEURL,
   timeout: 1000 * 60,
@@ -32,25 +35,17 @@ api.interceptors.request.use(
 
 api.interceptors.response.use(
   (response) => {
-    /**
-     * 全局拦截请求发送后返回的数据，如果数据有报错则在这做全局的错误提示
-     * 假设返回数据格式为：{ status: 1, error: '', data: '' }
-     * 规则是当 status 为 1 时表示请求成功，为 0 时表示接口需要登录或者登录状态失效，需要重新登录
-     * 请求出错时 error 会返回错误信息
-     */
-    if (response.data.status === 1) {
-      if (response.data.error !== '') {
-        // 错误提示
-        Message.error(response.data.error, {
-          zIndex: 2000,
-        })
-        return Promise.reject(response.data)
-      }
+    const { code, msg } = response.data as BaseResult
+    if (code === 200) {
+      return Promise.resolve(response.data)
     }
     else {
-      useUserStore().logout()
+      if (code === 401) {
+        useUserStore().logout()
+      }
+      ElMessage.error(msg || '服务器内部错误')
+      return Promise.reject(response.data)
     }
-    return Promise.resolve(response.data)
   },
   (error) => {
     let message = error.message
