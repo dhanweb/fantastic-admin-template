7baf403c3b9f2ec01a7794b9822c051515afa612
dhanweb <851849619@qq.com>
2024-06-25 13:44:27 +0800

    feat: pinia持久插件，设置前缀

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,10 @@
+import { createPersistedState } from 'pinia-plugin-persistedstate'
+import Settings from '@/settings.ts'
+
 const pinia = createPinia()
+pinia.use(createPersistedState ({
+  storage: window.localStorage,
+  key: id => `${Settings.app.prefix ?? ''}${id}`,
+}))
 
 export default pinia
